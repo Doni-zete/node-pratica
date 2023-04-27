7923195357f0263904bd79ddb736a8dfa822645b
doni-zete <donizete112@gmail.com>
2023-04-27 20:33:10 -0300

    modulos

---
+++ 02modulos/app.js
@@ -0,0 +1,12 @@
+//Importando calculos
+var calculos = require("./calculos");
+
+//Obtendo a função
+var somar = calculos.somar;
+var multiplicar = calculos.multiplicar;
+
+console.log(multiplicar(10, 2));
+
+console.log(somar(15, 36));
+
+//modulos são pequenas bibliotecas
